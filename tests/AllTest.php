7db7973cbabda4facfe7480b8fc501b6731a3991
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2015-07-14 09:06:58 +0900

    Update AllTest.php

--- tests/AllTest.php
+++ tests/AllTest.php
@@ -1,8 +1,15 @@
 <?php
 
+require(dirname(__FILE__) . '/../src/player.class.php');
+
 class AllTest extends PHPUnit_Framework_TestCase
 {
     public function testDefault()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test01
     {
         $this->assertTrue(true);
     }
