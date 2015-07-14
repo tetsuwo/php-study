a91ee4246a6a7dd3d37cb96803b5495359faf751
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2015-07-15 00:15:12 +0900

    Update AllTest.php

--- tests/AllTest.php
+++ tests/AllTest.php
@@ -16,7 +16,7 @@ class AllTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($me->getState() === Player::STATE_POISON);
 
         $me->setState($me->getState() | Player::STATE_PARALYSIS);
-        $this->assertTrue($me->getState() === Player::STATE_POISON);
+        $this->assertTrue($me->getState() === 18);
 
     }
 }
