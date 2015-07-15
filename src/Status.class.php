0d24fe9a12d647e866d5ecd78d08ee2d7d4a4702
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2015-07-16 02:06:37 +0900

    Create Status.class.php

---
+++ src/Status.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class Status {                  
+    const DEATH     = 0b000001; 
+    const POISON    = 0b000010; 
+    const SLEEP     = 0b000100; 
+    const PETRIFIED = 0b001000; 
+    const CONFUSION = 0b010000; 
+    const PARALYSIS = 0b100000; 
+}                               
