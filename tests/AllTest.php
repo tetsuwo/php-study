c7b0e3c06d71e0e8766a8aae18eb383ae1ce02e8
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2015-07-24 21:28:59 +0900

    Update AllTest.php

--- tests/AllTest.php
+++ tests/AllTest.php
@@ -49,7 +49,7 @@ class AllTest extends PHPUnit_Framework_TestCase
         $me->setState($me->getState() | Status::PARALYSIS);
         $me->setState($me->getState() | Status::CONFUSION);
         echo "\n2 ... "; var_dump($me->getState(), decbin($me->getState())); echo "\n";
-        echo "\n2 ... "; var_dump($me->getState() & ~Item::BANNOUYAKU, decbin($me->getState()& ~Item::BANNOUYAKU)); echo "\n";
+        echo "\n2 ... "; var_dump(1 & ~Item::BANNOUYAKU, decbin($me->getState()& ~Item::BANNOUYAKU)); echo "\n";
         $me->setState($me->getState() ^ Item::BANNOUYAKU);
         echo "\n3 ... "; var_dump($me->getState(), decbin($me->getState())); echo "\n";
         $this->assertTrue(
